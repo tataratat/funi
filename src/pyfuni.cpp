f889d3f83a2301c666ebff3dd9d942e5d9640cac
Jaewook Lee <jaewooklee042@gmail.com>
2023-06-26 19:49:16 +0200

    add cpp

---
+++ src/pyfuni.cpp
@@ -0,0 +1,9 @@
+#include <pybind11/numpy.h>
+#include <pybind11/pybind11.h>
+
+#include <pyfuni.hpp>
+
+PYBIND11_MODULE(funi, m) {
+  m.def("unique_float32", &funi::Unique<float>);
+  m.def("unique_float64", &funi::Unique<double>);
+}
